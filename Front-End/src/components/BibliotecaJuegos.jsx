a79856cfab3559bd391cc594520889a64d9f36a6
miguebaena <miguel.baena2009@gmail.com>
2025-10-29 16:57:33 -0500

    Se eliminan 2 archivos y se modifica biblioteca juegos

--- Front-End/src/components/BibliotecaJuegos.jsx
+++ Front-End/src/components/BibliotecaJuegos.jsx
@@ -1,9 +1,32 @@
-import "/Users/migue/Documents/Proyecto final/Front-End/src/styles/styles.css"
+import React from "react";
+import TarjetaJuego from "./TarjetaJuego.jsx";
+import FormularioJuego from "./FormularioJuego.jsx";
+import EstadisticasPersonales from "./EstadisticasPersonales.jsx";
+//import "C:/Users/migue/Documents/Proyecto final/Front-End/src/styles"
 
 function BibliotecaJuegos() {
+  return (
+    <section>
+      <div className="top-row">
+        <div>
+          <input placeholder="Buscar juegos..." />
+        </div>
+        <div>
+          <h3>Agregar nuevo juego</h3>
+          <FormularioJuego />
+        </div>
+      </div>
 
-    return (
-        <h1>bienvenido</h1>
-    )
-}
-export default BibliotecaJuegos
+      <EstadisticasPersonales />
+
+      <h2>Biblioteca</h2>
+      <div className="grid">
+        <TarjetaJuego />
+        <TarjetaJuego />
+        <TarjetaJuego />
+      </div>
+    </section>
+  );
+};
+
+export default BibliotecaJuegos;
