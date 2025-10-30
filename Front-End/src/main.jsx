5b087c67566e2e07d750de41a79755b3f938a1d6
miguebaena <miguel.baena2009@gmail.com>
2025-10-30 11:21:35 -0500

    Cambios minimos

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import "./styles/styles.css"
 
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-)
+  </React.StrictMode>
+);
