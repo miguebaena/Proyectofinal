36d81b1509b78347de595326a5c7a7f86164dfa3
miguebaena <miguel.baena2009@gmail.com>
2025-11-03 13:04:13 -0500

    Se agregan las estadisticas personales

---
+++ Front-End/src/components/EstadisticasPersonales.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const EstadisticasPersonales = () => {
+  return (
+    <div className="stats">
+      <div>🎮 Total juegos: 10</div>
+      <div>⏱️ Horas jugadas: 123</div>
+      <div>⭐ Promedio: 4.3</div>
+      <div>✅ Completados: 6</div>
+    </div>
+  );
+};
+
+export default EstadisticasPersonales;
